f387db1c1caabc2419eb7ee3ae8cf6d259284516
bengris32 <bengris32@protonmail.ch>
2024-06-26 17:54:17 +0100

    nashc: Specify screen size/density
    Change-Id: I7ac64b1da338ec09fb45f1acca62027e3ac8bb77
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -12,6 +12,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
