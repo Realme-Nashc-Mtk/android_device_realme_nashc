fa2b34836a470f3c85583e6175a2584df5e02a7d
bengris32 <bengris32@protonmail.ch>
2024-07-02 22:22:20 +0100

    nashc: Import Widevine DRM blobs
    Change-Id: I9b0187c552796289d170b2940e2728095b20f4d1
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -73,6 +73,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor:64
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
