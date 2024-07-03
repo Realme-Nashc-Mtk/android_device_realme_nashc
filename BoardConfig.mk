7eaa67c5ded46503d435d3cd832487d1dd05e697
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-07-03 17:50:56 +0100

    nashc: Enable vendor RIL service
    * We use Mediatek's RIL implementation
    
    Change-Id: I8ca9fcf2afd335ca8f9c7901efa74cddae34708f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,6 +123,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 
