986996282d15631e3247b3f032fe4d897b3345d6
bengris32 <bengris32@protonmail.ch>
2024-07-04 11:17:00 +0100

    nashc: Build Pixel thermal service
    * Config based off "dumpsys thermalservice" using stock thermal service.
    
    Change-Id: I92fcc16ce01aae8b1b9307c38f4236dffb7f78b9
    Co-authored-by: Woomymy <woomy@woomy.be>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -234,6 +234,13 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
