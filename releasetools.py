328184c92f0a3ef54f10bbafd95cb7ee7a1aaf53
bengris32 <bengris32@protonmail.ch>
2024-08-20 19:42:39 +0100

    nashc: releasetools: Update firmware during OTA
    * Flash all firmware images to their respective partitions during OTA.
    
    * Also drop unneeded 'return' statements.
    
    Change-Id: I2274ff4ac78bb84c6439331e7d4eb78a1071bdfc
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- releasetools.py
+++ releasetools.py
@@ -5,24 +5,52 @@
 
 import common
 
+FIRMWARE_IMAGES = {
+  "audio_dsp.img": "audio_dsp",
+  "cam_vpu1.img": "cam_vpu1",
+  "cam_vpu2.img": "cam_vpu2",
+  "cam_vpu3.img": "cam_vpu3",
+  "gz.img": "gz",
+  "lk.img": "lk",
+  "md1img.img": "md1img",
+  "preloader.img": "preloader",
+  "scp.img": "scp",
+  "spmfw.img": "spmfw",
+  "sspm.img": "sspm",
+  "tee.img": "tee"
+}
+
+def FullOTA_InstallBegin(info):
+  OTA_InstallBegin(info)
+
+def IncrementalOTA_InstallBegin(info):
+  OTA_InstallBegin(info)
+
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  return
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  return
 
-def AddImage(info, basename, dest):
+def AddImage(info, dir, basename, dest):
   name = basename
-  data = info.input_zip.read("IMAGES/" + basename)
+  data = info.input_zip.read(dir + basename)
   common.ZipWriteStr(info.output_zip, name, data)
+  FlashImage(info, name, dest)
+
+def FlashImage(info, name, dest):
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
+def OTA_InstallBegin(info):
+  info.script.Print("Updating firmware images...")
+  for name, part in FIRMWARE_IMAGES.items():
+    AddImage(info, "RADIO/", name, "/dev/block/by-name/" + part)
+
+  FlashImage(info, "preloader.img", "/dev/block/by-name/preloader_backup")
+
 def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
-  AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
-  AddImage(info, "vbmeta_vendor.img", "/dev/block/by-name/vbmeta_vendor")
-  return
+  AddImage(info, "IMAGES/", "dtbo.img", "/dev/block/by-name/dtbo")
+  AddImage(info, "IMAGES/", "vbmeta.img", "/dev/block/by-name/vbmeta")
+  AddImage(info, "IMAGES/", "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
+  AddImage(info, "IMAGES/", "vbmeta_vendor.img", "/dev/block/by-name/vbmeta_vendor")
