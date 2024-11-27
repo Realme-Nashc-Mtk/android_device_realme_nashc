334a480e078fe02e6241ac63828768b6c8e5f189
me-cafebabe <me.cafebabe@gmail.com>
2024-11-27 00:24:58 +0000

    nashc: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- device.mk
+++ device.mk
@@ -126,6 +126,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     ApertureOverlayNashc \
+    DeviceAsWebcamOverlayNashc \
     FrameworksResOverlayNashc \
     LineageSDKOverlayNashc \
     LineageSystemUIOverlayNashc \
