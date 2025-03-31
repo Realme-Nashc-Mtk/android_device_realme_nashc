1e52aaad28562ef8666fc4da98c889859e81c896
R0rt1z2 <rogerortizleal@gmail.com>
2025-03-31 15:58:22 +0200

    nashc: Hexpatch camera lib to increase maximum JPEG streams for an app
    * Increase maxAppJpegStreamNum from 5 to 11 in order to fix GCam lags.
    * evaluateCaptureConfiguration()

--- extract-files.py
+++ extract-files.py
@@ -62,6 +62,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libutils.so', 'libutils-v32.so'),
     'vendor/lib64/libmtkcam_stdutils.so': blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
+    'vendor/lib64/libmtkcam_featurepolicy.so': blob_fixup()
+        .binary_regex_replace(b'\x34\xE8\x87\x40\xB9', b'\x34\x28\x02\x80\x52'),
     ('vendor/bin/mnld', 'vendor/lib64/libaalservice.so', 'vendor/lib64/libcam.utils.sensorprovider.so', 'vendor/lib64/liboplus_mtkcam_lightsensorprovider.so'): blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib64/hw/audio.primary.mt6785.so': blob_fixup()
