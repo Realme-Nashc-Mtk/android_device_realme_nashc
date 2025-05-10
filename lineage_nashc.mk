b39de74c7578f9fd2a184299ea4dee66b743b1d4
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-05-10 06:28:33 +0530

    nashc: Enable UDFPS Addons

--- lineage_nashc.mk
+++ lineage_nashc.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/realme/nashc/device.mk)
 
+# Addons
+TARGET_HAS_UDFPS := true
+
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
